f243a784be2b64afd56ba1fd099e77f3ec3b2cb3
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2024-05-07 13:26:49 +0200

    Add log in file

--- functs_utils.js
+++ functs_utils.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 function logText(text) {
     const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ");
-    console.log(`[${formattedDate}] ${text}`);
+    const formattedDate = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+    const logMessage = `[${formattedDate}] ${text}`;
+
+    // Log to console
+    console.log(logMessage);
+
+    // Log to file
+    const logFilePath = path.join(__dirname, 'app.log'); // Define path to log file
+    fs.appendFileSync(logFilePath, logMessage + '\n'); // Append the log message to the log file
 }
 
 module.exports.logText = logText;
