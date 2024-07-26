101b33d5dd988132c8077ca0a8e69a49c8115051
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2024-07-26 21:50:48 +0200

    fix: Message

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const http = require('http');
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     var message = 'Bot is running\n';
-    var response = [message, version].join('\n');
-    res.end(response);
+    res.end(message);
 });
 
 server.listen(3000, () => {
