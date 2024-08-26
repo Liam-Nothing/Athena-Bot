a1cda785411eeb4046dc7a23e57816a5ebdde0f1
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2024-08-26 23:58:24 +0200

    feat: Add security

--- status.js
+++ status.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const url = require('url');
 const pm2 = require('pm2');
 const { exec } = require('child_process');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const botName = 'discord-bot';
+const apiKey = process.env.API_KEY;
 
 function checkBotStatus(callback) {
     pm2.connect((err) => {
@@ -42,22 +45,32 @@ function executeCommand(command, res) {
 }
 
 const server = http.createServer((req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    
+    if (queryObject.key !== apiKey) {
+        res.writeHead(403, { 'Content-Type': 'text/plain' });
+        res.end('Forbidden: Invalid API Key');
+        return;
+    }
+
     if (req.method === 'GET') {
-        if (req.url === '/status') {
+        if (req.url.startsWith('/status')) {
             checkBotStatus((status) => {
                 res.writeHead(200, { 'Content-Type': 'text/plain' });
                 res.end(`Bot status: ${status}`);
             });
-        } else if (req.url === '/start') {
+        } else if (req.url.startsWith('/start')) {
             executeCommand('npm run start', res);
-        } else if (req.url === '/stop') {
+        } else if (req.url.startsWith('/stop')) {
             executeCommand(`npm run stop`, res);
-        } else if (req.url === '/restart') {
+        } else if (req.url.startsWith('/restart')) {
             executeCommand(`npm run stop && npm run start`, res);
-        } else if (req.url === '/delete') {
+        } else if (req.url.startsWith('/delete')) {
             executeCommand(`npm run delete`, res);
-        } else if (req.url === '/list') {
+        } else if (req.url.startsWith('/list')) {
             executeCommand(`npm run listing`, res);
+        } else if (req.url.startsWith('/pull')) {
+            executeCommand(`npm run pull`, res);
         } else {
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('Not Found');
